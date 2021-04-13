8224765f003cff15eb5cc17c1130b2f2bafacc9b
lidongjies <lidongjies@gmail.com>
2021-04-13 19:02:53 +0800

    feat: add audio media support

--- dist/miniprogram-adapter.d.ts
+++ dist/miniprogram-adapter.d.ts
@@ -1,5 +1,6 @@
+import { default as PIXI_2 } from '@eva/miniprogram-pixi';
 
-declare const _default: any;
+declare const _default: typeof PIXI_2.miniprogram;
 export default _default;
 
 export { }
