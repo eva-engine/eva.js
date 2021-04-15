124d4fda26867fec3cb11ec0455b8835e7537e77
lidongjies <lidongjies@gmail.com>
2021-04-15 18:41:11 +0800

    fix: export pixi.js obj from miniprogram-pixi

--- dist/miniprogram-pixi.d.ts
+++ dist/miniprogram-pixi.d.ts
@@ -1,4 +1,20 @@
+import { Application } from '@tbminiapp/pixi-miniprogram-engine';
+import { Container } from '@tbminiapp/pixi-miniprogram-engine';
+import { extras } from '@tbminiapp/pixi-miniprogram-engine';
+import { Graphics } from '@tbminiapp/pixi-miniprogram-engine';
+import { mesh } from '@tbminiapp/pixi-miniprogram-engine';
 import * as PIXI_2 from '@tbminiapp/pixi-miniprogram-engine';
+import { Sprite } from '@tbminiapp/pixi-miniprogram-engine';
+import { Text as Text_2 } from '@tbminiapp/pixi-miniprogram-engine';
+import { Texture } from '@tbminiapp/pixi-miniprogram-engine';
+export { Application }
+export { Container }
+export { extras }
+export { Graphics }
+export { mesh }
 export default PIXI_2;
+export { Sprite }
+export { Text_2 as Text }
+export { Texture }
 
 export { }
--- packages/miniprogram-pixi/lib/index.ts
+++ packages/miniprogram-pixi/lib/index.ts
@@ -1,3 +1,3 @@
 import * as PIXI from '@tbminiapp/pixi-miniprogram-engine';
-
+export { Application, Container, Graphics, mesh, Texture, Sprite, extras, Text } from '@tbminiapp/pixi-miniprogram-engine';
 export default PIXI;
