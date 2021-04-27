73c3d3e10aba0643eaf4d0d45b098f63785863b2
lidongjies <lidongjies@gmail.com>
2021-04-27 16:53:59 +0800

    doc: update the readme files of all packages

--- packages/miniprogram-pixi/index.js
+++ packages/miniprogram-pixi/index.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/miniprogram-pixi.cjs.prod.js');
+  module.exports = require('./dist/miniprogram-pixi.cjs.prod.js')
 } else {
-  module.exports = require('./dist/miniprogram-pixi.cjs.js');
+  module.exports = require('./dist/miniprogram-pixi.cjs.js')
 }
--- packages/plugin-matterjs/README.md
+++ packages/plugin-matterjs/README.md
@@ -1,1 +1,7 @@
+
 # @eva/plugin-matterjs
+
+More Introduction
+- [EN](https://eva.js.org)
+- [中文](https://eva-engine.gitee.io)
+    
