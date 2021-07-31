5c57ab5e3d443438f46eadf78ca89f7e0692a24c
fanmingfei <az8641683@163.com>
2021-07-31 21:55:50 +0800

    fix: memory

--- packages/plugin-renderer-img/lib/system.ts
+++ packages/plugin-renderer-img/lib/system.ts
@@ -81,6 +81,7 @@ export default class Img extends Renderer {
         this.containerManager
           .getContainer(changed.gameObject.id)
           .removeChild(sprite.sprite);
+          sprite.sprite.destroy(true)
         delete this.imgs[changed.gameObject.id];
       }
     }
