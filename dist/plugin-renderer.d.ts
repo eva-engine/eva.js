8ed4ae2b72f703b965a2b443688b3bbefa53ec73
李冬杰 <lidongjies@gmail.com>
2021-08-06 15:50:43 +0800

    merge dev1.1 (#87)
    * Merge dev. (#54)
    * feat: enhancement spriteAnimation (#52)
    - Add gotoAndPlay & gotoAndStop for AnimationSprite. #51
    - Adding play times for SpriteAnimation #38
    * fix: lost frame bug #25 (#50)
    * doc: add Chinese reademe
    * doc: update reademe
    * doc: Update reademe
    * Feat/resize (#67)
    * feat: change size #24
    * fix: touchmove event #10
    - update type definition for renderer 
    - fix: moveWhenInside params type
    - fix: catch setting moveWhenInside error
    * feat: component generics (#75)
    * update rollup config
    * fix babel-runtime error in eva.js
    * release: v1.0.12-alpha.2
    * test: update test suit
    * chore: fix dependencies in release script
    * release: v1.1.0-alpha.0
    * fix: dragonbone component params and renderer system init params
    * release: v1.1.0-alpha.1
    * Change the construction parameters of system to optional
    * remove spine38 and loader example
    Co-authored-by: Mingfei <az8641683@163.com>

--- dist/plugin-renderer.d.ts
+++ dist/plugin-renderer.d.ts
@@ -11,96 +11,95 @@ import { System } from '@eva/eva.js';
 import { Transform } from '@eva/eva.js';
 
 export declare class ContainerManager {
-    containerMap: {
-        [propName: number]: Container;
-    };
-    addContainer({ name, container }: {
-        name: number;
-        container: Container;
-    }): void;
-    getContainer(name: number): Container;
-    removeContainer(name: number): void;
-    updateTransform({ name, transform }: {
-        name: number;
-        transform: Transform;
-    }): void;
+  containerMap: {
+    [propName: number]: Container;
+  };
+  addContainer({ name, container }: {
+    name: number;
+    container: Container;
+  }): void;
+  getContainer(name: number): Container;
+  removeContainer(name: number): void;
+  updateTransform({ name, transform }: {
+    name: number;
+    transform: Transform;
+  }): void;
 }
 
 export declare class Renderer extends System {
-    name: string;
-    game: Game;
-    static observerInfo: PureObserverInfo;
-    observerInfo: PureObserverInfo;
-    containerManager: ContainerManager;
-    rendererManager: RendererManager;
-    constructor(params?: {});
-    init?(arg?: any): void;
-    componentChanged?(changed: ComponentChanged): void;
-    rendererUpdate?(gameObject: GameObject): void;
-    update(): void;
+  name: string;
+  game: Game;
+  static observerInfo: PureObserverInfo;
+  observerInfo: PureObserverInfo;
+  containerManager: ContainerManager;
+  rendererManager: RendererManager;
+  constructor(params?: any);
+  componentChanged(_changed: ComponentChanged): void;
+  rendererUpdate(_gameObject: GameObject): void;
+  update(): void;
 }
 
 export declare enum RENDERER_TYPE {
-    UNKNOWN = 0,
-    WEBGL = 1,
-    CANVAS = 2
+  UNKNOWN = 0,
+  WEBGL = 1,
+  CANVAS = 2
 }
 
 export declare class RendererManager {
-    game: Game;
-    rendererSystem: RendererSystem;
-    constructor({ game, rendererSystem }: {
-        game: any;
-        rendererSystem: any;
-    });
-    renderers: Renderer[];
-    register(...renderers: Renderer[]): void;
-    componentChanged(changes: ComponentChanged[]): void;
-    update(gameObject: GameObject): void;
+  game: Game;
+  rendererSystem: RendererSystem;
+  constructor({ game, rendererSystem }: {
+    game: any;
+    rendererSystem: any;
+  });
+  renderers: Renderer[];
+  register(...renderers: Renderer[]): void;
+  componentChanged(changes: ComponentChanged[]): void;
+  update(gameObject: GameObject): void;
 }
 
 export declare class RendererSystem extends System {
-    static systemName: string;
+  static systemName: string;
+  params: any;
+  rendererManager: RendererManager;
+  containerManager: ContainerManager;
+  application: Application;
+  game: Game;
+  transform: Transform_2;
+  multiApps: Application[];
+  init(params: any): void;
+  registerObserver(observerInfo: any): void;
+  createMultiApplication({ params }: {
     params: any;
-    rendererManager: RendererManager;
-    containerManager: ContainerManager;
-    application: Application;
-    game: Game;
-    transform: Transform_2;
-    multiApps: Application[];
-    init(params: any): void;
-    registerObserver(observerInfo: any): void;
-    createMultiApplication({ params }: {
-        params: any;
-    }): Application;
-    createApplication(params: any): Application;
-    update(): void;
-    lateUpdate(e: any): void;
-    onDestroy(): void;
-    resize(width: any, height: any): void;
+  }): Application;
+  createApplication(params: any): Application;
+  update(): void;
+  lateUpdate(e: any): void;
+  onDestroy(): void;
+  resize(width: any, height: any): void;
 }
 
 declare class Transform_2 extends EventEmitter {
-    name: string;
-    waitRemoveIds: number[];
-    waitSceneId: number;
-    system: RendererSystem;
-    containerManager: ContainerManager;
-    waitChangeScenes: {
-        scene: Scene;
-        mode: LOAD_SCENE_MODE;
-        application: Application;
-    }[];
-    constructor({ system, containerManager }: {
-        system: any;
-        containerManager: any;
-    });
-    init(system: RendererSystem): void;
-    update(): void;
-    componentChanged(changed: ComponentChanged): void;
-    addContainer(changed: ComponentChanged): void;
-    change(changed: ComponentChanged): void;
-    destroy(): void;
+  name: string;
+  waitRemoveIds: number[];
+  waitSceneId: number;
+  system: RendererSystem;
+  containerManager: ContainerManager;
+  waitChangeScenes: {
+    scene: Scene;
+    mode: LOAD_SCENE_MODE;
+    application: Application;
+  }[];
+  constructor({ system, containerManager }: {
+    system: any;
+    containerManager: any;
+  });
+  init(system: RendererSystem): void;
+  update(): void;
+  componentChanged(changed: ComponentChanged): void;
+  addContainer(changed: ComponentChanged): void;
+  change(changed: ComponentChanged): void;
+  destroy(): void;
 }
 
 export { }
