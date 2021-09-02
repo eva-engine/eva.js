b7e4315355932ad43186b97e8df4236583391587
Mingfei <az8641683@163.com>
2021-09-02 22:50:36 +0800

    Fix/1.1.6 cr (#129)
    * fix: animation event
    
    * fix: multi scene params interface
    
    * fix: types/matterjs dependencies
    
    * fix: application params options

--- packages/renderer-adapter/lib/Application.ts
+++ packages/renderer-adapter/lib/Application.ts
@@ -2,7 +2,7 @@ import { Application as PIXIApplication } from 'pixi.js';
 import type { ApplicationOptions } from "pixi.js";
 
 export default class Application extends PIXIApplication {
-  constructor(params?: ApplicationOptions) {
+  constructor(params: ApplicationOptions) {
     params.autoStart = false;
     super(params);
   }
