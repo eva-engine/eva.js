3bc9ca19765e5a2c056736a297d8ac2eb610498b
fanmingfei <az8641683@163.com>
2021-10-02 16:23:49 +0800

    fix: useless

--- examples/src/a11y.ts
+++ examples/src/a11y.ts
@@ -381,7 +381,8 @@ export async function init(canvas) {
       new SpineSystem(),
       new A11ySystem({
         debug: true,
-        activate: A11yActivate.CHECK,      }),
+        activate: A11yActivate.CHECK,
+      }),
     ],
     autoStart: true,
     frameRate: 60,
