5ce44c7b14eb689f57b5c0be70bbf5712c4cee12
fanmingfei <az8641683@163.com>
2021-12-29 17:09:36 +0800

    feat: gameObject addChildAt

--- packages/eva.js/lib/core/Transform.ts
+++ packages/eva.js/lib/core/Transform.ts
@@ -1,4 +1,4 @@
-import {type, step} from '@eva/inspector-decorator';
+import { type, step } from '@eva/inspector-decorator';
 import Component from './Component';
 import type { ComponentParams } from './Component';
 
@@ -56,6 +56,9 @@ class Transform extends Component<TransformParams> {
   readonly name: string = 'Transform';
   private _parent: Transform = null;
 
+  /** only used in addChildAt */
+  childIndex: number = -1;
+
   /** Whether this transform in a scene object */
   inScene: boolean = false;
 
@@ -77,12 +80,12 @@ class Transform extends Component<TransformParams> {
     this.rotation = params.rotation || this.rotation;
   }
 
-  @type('vector2') @step(1) position: Vector2 = {x: 0, y: 0};
-  @type('size') @step(1) size: Size2 = {width: 0, height: 0};
-  @type('vector2') @step(0.1) origin: Vector2 = {x: 0, y: 0};
-  @type('vector2') @step(0.1) anchor: Vector2 = {x: 0, y: 0};
-  @type('vector2') @step(0.1) scale: Vector2 = {x: 1, y: 1};
-  @type('vector2') @step(0.1) skew: Vector2 = {x: 0, y: 0};
+  @type('vector2') @step(1) position: Vector2 = { x: 0, y: 0 };
+  @type('size') @step(1) size: Size2 = { width: 0, height: 0 };
+  @type('vector2') @step(0.1) origin: Vector2 = { x: 0, y: 0 };
+  @type('vector2') @step(0.1) anchor: Vector2 = { x: 0, y: 0 };
+  @type('vector2') @step(0.1) scale: Vector2 = { x: 1, y: 1 };
+  @type('vector2') @step(0.1) skew: Vector2 = { x: 0, y: 0 };
   @type('number') @step(0.1) rotation: number = 0;
 
   set parent(val: Transform) {
@@ -108,14 +111,19 @@ class Transform extends Component<TransformParams> {
    * @param child - child gameObject's transform component
    */
   addChild(child: Transform) {
-    if (child.parent === this) {
-      const index = this.children.findIndex(item => item === child);
-      this.children.splice(index, 1);
-    } else if (child.parent) {
+    if (child.parent) {
       child.parent.removeChild(child);
     }
     child._parent = this;
-    this.children.push(child);
+    if (child.childIndex > -1) {
+
+      // packages/eva.js/lib/core/GameObject.ts > addChildAt
+      // packages/plugin-renderer/lib/Transform.ts > change
+      // packages/eva.js/lib/core/Transform.ts > addChild
+      this.children.splice(child.childIndex, 0, child);
+    } else {
+      this.children.push(child);
+    }
   }
 
   /**
