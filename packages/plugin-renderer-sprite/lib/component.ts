854a7275bf01569454c9bae6e3c32f642b0b2ff7
jimizai <359743984@qq.com>
2022-05-16 17:41:25 +0800

    feat: support select dynamic fields

--- packages/plugin-renderer-sprite/lib/component.ts
+++ packages/plugin-renderer-sprite/lib/component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@eva/eva.js';
+import { Component, resource } from '@eva/eva.js';
 import { Field } from '@eva/inspector-decorator';
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 export interface SpriteParams {
   resource: string;
@@ -8,8 +10,28 @@ export interface SpriteParams {
 
 export default class Sprite extends Component<SpriteParams> {
   static componentName: string = 'Sprite';
-  @Field() resource: string = '';
-  @Field() spriteName: string = '';
+
+  @Field({ type: 'resource' })
+  resource: string = '';
+
+  @Field({
+    type: 'selector',
+    options: async function (instance: Sprite) {
+      await sleep(0);
+      if (!instance.resource || !(resource as any).promiseMap[instance.resource]) {
+        return {};
+      }
+      await (resource as any).promiseMap[instance.resource];
+      const frames = resource.resourcesMap[instance.resource]?.data?.json?.frames;
+      return frames
+        ? Object.values(frames)
+            .map((item: any) => item.name)
+            .reduce((prev, key) => ({ ...prev, [key]: key }), {})
+        : {};
+    },
+  })
+  spriteName: string = '';
+
   init(obj?: SpriteParams) {
     if (obj && obj.resource) {
       this.resource = obj.resource;
--- packages/spine-base/lib/Spine.ts
+++ packages/spine-base/lib/Spine.ts
@@ -1,4 +1,4 @@
-import { Component } from '@eva/eva.js';
+import { Component, resource } from '@eva/eva.js';
 import { Field } from '@eva/inspector-decorator';
 
 export interface SpineParams {
@@ -7,13 +7,26 @@ export interface SpineParams {
   autoPlay?: boolean;
 }
 
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
 export default class Spine extends Component<SpineParams> {
   static componentName: string = 'Spine';
 
   @Field({ type: 'resource' })
   resource: string = '';
 
-  @Field()
+  @Field({
+    type: 'selector',
+    options: async function (that: Spine) {
+      await sleep(0);
+      if (!that.resource || !(resource as any).promiseMap[that.resource]) {
+        return {};
+      }
+      await (resource as any).promiseMap[that.resource];
+      const animations = resource.resourcesMap[that.resource]?.data?.ske?.animations;
+      return animations ? Object.keys(animations).reduce((prev, key) => ({ ...prev, [key]: key }), {}) : {};
+    },
+  })
   animationName: string = '';
 
   @Field()
