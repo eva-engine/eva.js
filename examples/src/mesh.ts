381c9c48db56ba63858619c17066b4d2ed027144
jimizai <359743984@qq.com>
2024-12-26 15:08:06 +0800

    release: v2.0.0-beta.15

--- examples/src/mesh.ts
+++ examples/src/mesh.ts
@@ -33,8 +33,8 @@ export async function init() {
   const gameObj1 = new GameObject('symbol_1', {
     size: { width: 200, height: 244 },
     position: {
-      x: 100,
-      y: 100,
+      x: 0,
+      y: 0,
     },
   });
 
@@ -46,32 +46,70 @@ export async function init() {
 
   gameObj1.addComponent(meshComp);
 
-  setTimeout(async () => {
-    const res = await resource.getResource('meshName');
-    const texture = res?.data.image;
-    if (texture) {
-      const points = [
-        { x: 0, y: 0 },
-        { x: texture.width, y: 0 },
-        { x: texture.width, y: texture.height },
-        { x: 0, y: texture.height },
-      ];
+  const res = await resource.getResource('meshName');
+  const texture = res?.data.image;
+  if (texture) {
+    const points = [
+      { x: 0, y: 0 },
+      { x: texture.width, y: 0 },
+      { x: texture.width, y: texture.height },
+      { x: 0, y: texture.height },
+    ];
 
-      let outputPoints = points.map(point => ({ ...point, x: point.x + 50 }));
+    let outPoints = points.map(point => ({ ...point }));
+    function rotate3D(points, outPoints, angleX, angleY, perspective, isMirrorX) {
+      const radX = (angleX * Math.PI) / 180;
+      const radY = (angleY * Math.PI) / 180;
+      const cosX = Math.cos(radX);
+      const sinX = Math.sin(radX);
+      const cosY = Math.cos(radY);
+      const sinY = Math.sin(radY);
 
-      console.log(outputPoints);
-      meshComp.setCorners(
-        outputPoints[0].x,
-        outputPoints[0].y,
-        outputPoints[1].x,
-        outputPoints[1].y,
-        outputPoints[2].x,
-        outputPoints[2].y,
-        outputPoints[3].x,
-        outputPoints[3].y,
-      );
+      for (let i = 0; i < points.length; i++) {
+        const src = points[i];
+        const out = outPoints[i];
+        let x = src.x - texture.width / 2;
+        if (isMirrorX) {
+          x = -x;
+        }
+        const y = src.y - texture.height / 2;
+        let z = 0; // Assume initial z is 0 for this 2D plane
+
+        // Rotate around Y axis
+        const xY = cosY * x - sinY * z;
+
+        z = sinY * x + cosY * z;
+
+        // Rotate around X axis
+        const yX = cosX * y - sinX * z;
+
+        z = sinX * y + cosX * z;
+
+        // Apply perspective projection
+        const scale = perspective / (perspective - z);
+
+        out.x = xY * scale + texture.width / 2;
+        out.y = yX * scale + texture.height / 2;
+      }
     }
-  }, 5000);
+    let angleY = 0;
+
+    setInterval(() => {
+      angleY += 1;
+      rotate3D(points, outPoints, 0, angleY, 300, false);
+      // const vertices = outPoints.reduce((prev, { x, y }) => [...prev, x, y], []);
+      meshComp.setCorners(
+        outPoints[0].x,
+        outPoints[0].y,
+        outPoints[1].x,
+        outPoints[1].y,
+        outPoints[2].x,
+        outPoints[2].y,
+        outPoints[3].x,
+        outPoints[3].y,
+      );
+    }, 10);
+  }
 
   game.scene.addChild(gameObj1);
 }
