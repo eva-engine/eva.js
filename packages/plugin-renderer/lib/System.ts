973d65f9bcea9066d1a9ee016f70ba9d34fdd55d
jimizai <359743984@qq.com>
2025-10-23 16:35:12 +0800

    fix: add TypeScript ignore comment for renderer resize method

--- packages/plugin-renderer/lib/System.ts
+++ packages/plugin-renderer/lib/System.ts
@@ -162,6 +162,7 @@ export default class Renderer extends System<RendererSystemParams> {
   resize(width, height) {
     this.params.width = width;
     this.params.height = height;
+    // @ts-ignore
     this.application.renderer.resize(width, height);
   }
 
